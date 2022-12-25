09f854404f00654b3531fb06bd0b557a142e6233
Talha Demir <talha.demir06@gmail.com>
2022-12-25 22:48:36 +0300

    first class created

--- src/test/java/week1/FirstSeleniumClass.java
+++ src/test/java/week1/FirstSeleniumClass.java
@@ -1,2 +1,23 @@
-package week1;public class FirstSeleniumClass {
+package week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class FirstSeleniumClass {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.operadriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://practice.cydeo.com/");
+
+        WebDriver driver1 = new OperaDriver();
+
+        driver1.get("https://practice.cydeo.com/");
+
+    }
 }
